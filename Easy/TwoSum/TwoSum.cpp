227e83ba41475941e125acad52953608e7bed95d
aktienautobahn <78978789+aktienautobahn@users.noreply.github.com>
2024-08-11 11:01:32 +0200

    adjusted the code

--- Easy/TwoSum/TwoSum.cpp
+++ Easy/TwoSum/TwoSum.cpp
@@ -4,15 +4,17 @@ public:
     size_t sum = 0; // init 0
     vector<int> result;
 
-    size_t size = nums.length();
+    size_t size = nums.size();
     for (size_t i = 0; i < size; i++) {
-      for (size_t j = i; j < size; j++ ) {
+      for (size_t j = i + 1; j < size; j++ ) {
         sum = nums[i] + nums[j];
         if (sum == target) {
-          result.push_back(nums[i]);
-          result.push_back(nums[j]);
+          result.push_back(i);
+          result.push_back(j);
+          return result;
         }
       }
     }
+    return result;
   }
 }
